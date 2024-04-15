2a3e212104619908cd38c7d4946ee2c1cfd45e88
Andrew <andrew@andrewsmith.org>
2024-04-15 02:54:09 -0400

    Add checkbox elements to item components

--- app/_components/Item.tsx
+++ app/_components/Item.tsx
@@ -1,11 +1,18 @@
 import { ItemType } from "../_interfaces/Bronze.interface";
+import { useState } from "react";
 
 export default function Item({ item }: { item: ItemType }) {
+  const [checked, setChecked] = useState(true);
+
   return (
-    <div className="border border-pink-800">
+    <div
+      className="border border-pink-800 flex justify-between"
+      onClick={() => setChecked(!checked)}
+    >
       <p>
         {item.name}: {item.cost}
       </p>
+      <input type="checkbox" name="" id="" checked={checked} />
     </div>
   );
 }
