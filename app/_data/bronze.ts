5635b46cd422d0373c77ede29e25c4f1f1b1a381
Andrew <andrew@andrewsmith.org>
2024-04-15 22:14:07 -0400

    Populate bronze.ts with two real vendors

--- app/_data/bronze.ts
+++ app/_data/bronze.ts
@@ -3,27 +3,347 @@ import { BronzeType } from "../_interfaces/Bronze.interface";
 const bronze: BronzeType = {
   vendors: [
     {
-      name: "vendor 1",
+      name: "Aeonicus",
       items: [
         {
-          name: "item 1",
-          cost: 300,
+          name: "Regalia of the Burning Scroll",
+          cost: 5000,
           id: 0,
+        },
+        {
+          name: "Regalia of the Chromatic Hydra",
+          cost: 5000,
+          id: 1,
+        },
+        {
+          name: "Chronomancer Regalia",
+          cost: 5000,
+          id: 2,
+        },
+        {
+          name: "Guardian Serpent Regalia",
+          cost: 5000,
+          id: 3,
+        },
+        {
+          name: "Regalia of the Exorcist",
+          cost: 5000,
+          id: 4,
+        },
+        {
+          name: "Regalia of Ternion Glory",
+          cost: 5000,
+          id: 5,
+        },
+        {
+          name: "Sha Skin Regalia",
+          cost: 5000,
+          id: 6,
+        },
+        {
+          name: "Regalia of the Thousandfold Hells",
+          cost: 5000,
+          id: 7,
+        },
+        {
+          name: "Regalia of the Horned Nightmare",
+          cost: 5000,
+          id: 8,
+        },
+        {
+          name: "Vestments of the Eternal Blossom",
+          cost: 5000,
+          id: 9,
+        },
+        {
+          name: "Vestments of the Haunted Forest",
+          cost: 5000,
+          id: 10,
+        },
+        {
+          name: "Vestments of the Shattered Vale",
+          cost: 5000,
+          id: 11,
+        },
+        {
+          name: "Vestments of the Red Crane",
+          cost: 5000,
+          id: 12,
+        },
+        {
+          name: "Vestments of the Seven Sacred Seals",
+          cost: 5000,
+          id: 13,
+        },
+        {
+          name: "Fire-Charm Vestments",
+          cost: 5000,
+          id: 14,
+        },
+        {
+          name: "Battlegear of the Thousandfold Blades",
+          cost: 5000,
+          id: 15,
+        },
+        {
+          name: "Nine-Tail Battlegear",
+          cost: 5000,
+          id: 16,
+        },
+        {
+          name: "Barbed Assassin Battlegear",
+          cost: 5000,
+          id: 17,
+        },
+        {
+          name: "Yuangol Slayer Battlegear",
+          cost: 5000,
+          id: 18,
+        },
+        {
+          name: "Battlegear of the Saurok Stalker",
+          cost: 5000,
+          id: 19,
+        },
+        {
+          name: "Battlegear of the Unblinking Vigil",
+          cost: 5000,
+          id: 20,
+        },
+        {
+          name: "Regalia of the Firebird",
+          cost: 5000,
+          id: 21,
+        },
+        {
+          name: "Regalia of the Witch Doctor",
+          cost: 5000,
+          id: 22,
+        },
+        {
+          name: "Regalia of Celestial Harmony",
+          cost: 5000,
+          id: 23,
+        },
+        {
+          name: "Plate of the Lost Catacomb",
+          cost: 5000,
+          id: 24,
+        },
+        {
+          name: "Battleplate of the All-Consuming Maw",
+          cost: 5000,
+          id: 25,
+        },
+        {
+          name: "Battleplate of the Cyclopean Dread",
+          cost: 5000,
+          id: 26,
+        },
+        {
+          name: "White Tiger Battlegear",
+          cost: 5000,
+          id: 27,
+        },
+        {
+          name: "Battlegear of the Lightning Emperor",
+          cost: 5000,
+          id: 28,
+        },
+        {
+          name: "Vestments of Winged Triumph",
+          cost: 5000,
+          id: 29,
+        },
+        {
+          name: "Battleplate of Resounding Rings",
+          cost: 5000,
+          id: 30,
+        },
+        {
+          name: "Battleplate of the Last Mogu",
+          cost: 5000,
+          id: 31,
+        },
+        {
+          name: "Battleplate of the Prehistoric Marauder",
+          cost: 5000,
+          id: 32,
         },
       ],
     },
     {
-      name: "vendor 2",
+      name: "Durus",
       items: [
         {
-          name: "item 1",
-          cost: 300,
-          id: 1,
+          name: "Regalia of the Burning Scroll",
+          cost: 5000,
+          id: 33,
         },
         {
-          name: "item 2",
-          cost: 300,
-          id: 2,
+          name: "Regalia of the Chromatic Hydra",
+          cost: 5000,
+          id: 34,
+        },
+        {
+          name: "Chronomancer Regalia",
+          cost: 5000,
+          id: 35,
+        },
+        {
+          name: "Guardian Serpent Regalia",
+          cost: 5000,
+          id: 36,
+        },
+        {
+          name: "Regalia of the Exorcist",
+          cost: 5000,
+          id: 37,
+        },
+        {
+          name: "Regalia of Ternion Glory",
+          cost: 5000,
+          id: 38,
+        },
+        {
+          name: "Sha Skin Regalia",
+          cost: 5000,
+          id: 39,
+        },
+        {
+          name: "Regalia of the Thousandfold Hells",
+          cost: 5000,
+          id: 40,
+        },
+        {
+          name: "Regalia of the Horned Nightmare",
+          cost: 5000,
+          id: 41,
+        },
+        {
+          name: "Vestments of the Eternal Blossom",
+          cost: 5000,
+          id: 42,
+        },
+        {
+          name: "Vestments of the Haunted Forest",
+          cost: 5000,
+          id: 43,
+        },
+        {
+          name: "Vestments of the Shattered Vale",
+          cost: 5000,
+          id: 44,
+        },
+        {
+          name: "Vestments of the Red Crane",
+          cost: 5000,
+          id: 45,
+        },
+        {
+          name: "Vestments of the Seven Sacred Seals",
+          cost: 5000,
+          id: 46,
+        },
+        {
+          name: "Fire-Charm Vestments",
+          cost: 5000,
+          id: 47,
+        },
+        {
+          name: "Battlegear of the Thousandfold Blades",
+          cost: 5000,
+          id: 48,
+        },
+        {
+          name: "Nine-Tail Battlegear",
+          cost: 5000,
+          id: 49,
+        },
+        {
+          name: "Barbed Assassin Battlegear",
+          cost: 5000,
+          id: 50,
+        },
+        {
+          name: "Yuangol Slayer Battlegear",
+          cost: 5000,
+          id: 51,
+        },
+        {
+          name: "Battlegear of the Saurok Stalker",
+          cost: 5000,
+          id: 52,
+        },
+        {
+          name: "Battlegear of the Unblinking Vigil",
+          cost: 5000,
+          id: 53,
+        },
+        {
+          name: "Regalia of the Firebird",
+          cost: 5000,
+          id: 54,
+        },
+        {
+          name: "Regalia of the Witch Doctor",
+          cost: 5000,
+          id: 55,
+        },
+        {
+          name: "Regalia of Celestial Harmony",
+          cost: 5000,
+          id: 56,
+        },
+        {
+          name: "Plate of the Lost Catacomb",
+          cost: 5000,
+          id: 57,
+        },
+        {
+          name: "Battleplate of the All-Consuming Maw",
+          cost: 5000,
+          id: 58,
+        },
+        {
+          name: "Battleplate of the Cyclopean Dread",
+          cost: 5000,
+          id: 59,
+        },
+        {
+          name: "White Tiger Battlegear",
+          cost: 5000,
+          id: 60,
+        },
+        {
+          name: "Battlegear of the Lightning Emperor",
+          cost: 5000,
+          id: 61,
+        },
+        {
+          name: "Vestments of Winged Triumph",
+          cost: 5000,
+          id: 62,
+        },
+        {
+          name: "Battleplate of Resounding Rings 1",
+          cost: 5000,
+          id: 63,
+        },
+        {
+          name: "Battleplate of Resounding Rings 2",
+          cost: 5000,
+          id: 64,
+        },
+        {
+          name: "Battleplate of the Last Mogu",
+          cost: 5000,
+          id: 65,
+        },
+        {
+          name: "Battleplate of the Prehistoric Marauder",
+          cost: 5000,
+          id: 66,
         },
       ],
     },
