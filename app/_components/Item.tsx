95ba32e93677f5546af52c5ffd5469f2c2513125
Andrew <andrew@andrewsmith.org>
2024-04-18 18:49:44 -0400

    Develop checkbox styling

--- app/_components/Item.tsx
+++ app/_components/Item.tsx
@@ -9,12 +9,18 @@ export default function Item({ item }: { item: ItemType }) {
 
   return (
     <div
-      className="flex justify-between odd:bg-emerald-700 even:bg-emerald-800"
+      className="flex justify-between odd:bg-emerald-700 even:bg-emerald-800 select-none cursor-pointer"
       onClick={handleItemClick}
     >
-      <p>
-        {item.name}: {item.cost}
-      </p>
+      <div className="">
+        <p>
+          {item.name}: {item.cost}
+        </p>
+        <p>
+          {/* PH -- going to add additional text/info about each item */}
+          {item.name}: {item.cost}
+        </p>
+      </div>
       <input
         type="checkbox"
         name=""
@@ -22,6 +28,7 @@ export default function Item({ item }: { item: ItemType }) {
         readOnly
         // operation below works fine without the OR condition, but makes react throw an error if checkedMap[item.id] produces a falsy value instead of a bool
         checked={appState.checkedMap[item.id] || false}
+        className="min-w-8 min-h-8 mx-2 my-auto  text-emerald-900 rounded-md cursor-pointer"
       />
     </div>
   );
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -15,6 +15,10 @@ const config: Config = {
       },
     },
   },
-  plugins: [],
+  plugins: [
+    require("@tailwindcss/forms")({
+      strategy: "base",
+    }),
+  ],
 };
 export default config;
