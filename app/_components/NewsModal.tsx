a256031bb25108b1de56b7834f0b11f4d6ebfff8
Andrew <andrew@andrewsmith.org>
2024-05-21 19:08:13 -0400

    Develop news modal styling

--- app/_components/NewsModal.tsx
+++ app/_components/NewsModal.tsx
@@ -38,17 +38,17 @@ export default function NewsModal() {
         className="fixed inset-0 bg-neutral-900/40 z-40 animate-modal-bg-fade"
         onClick={closeModal}
       ></div>
-      <div className="fixed top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2 w-4/5 md:w-3/4 lg:w-1/3 bg-emerald-600 rounded-xl z-50 shadow-lg animate-modal-appear">
-        <h1 className="bg-emerald-700 rounded-t-xl text-xl font-bold p-4">
-          hello it's the news
+      <div className="fixed top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2 w-4/5 md:w-3/4 xl:w-1/2 bg-emerald-600 rounded-xl z-50 shadow-lg animate-modal-appear">
+        <h1 className="bg-emerald-700 rounded-t-xl text-2xl text-center font-bold p-4">
+          What's new?
         </h1>
-        <div className="flex flex-col gap-4 text-lg">
+        <div className="flex flex-col">
           <div className="overflow-auto max-h-[60svh]">{newsItems}</div>
           <button
             onClick={closeModal}
-            className="m-4 px-4 py-1 text-lg font-semibold bg-emerald-400 hover:bg-emerald-500 active:bg-emerald-600 rounded-md"
+            className="m-4 px-4 py-1 text-lg font-semibold bg-emerald-700 hover:bg-emerald-800 active:bg-emerald-900 rounded-md mx-auto"
           >
-            thank you for news
+            Got it, thanks!
           </button>
         </div>
       </div>
