9c57f17a01e96f0f223ef3c4d76c6d7aad1199b4
Dillon Welch <daw0328@gmail.com>
2024-05-27 00:35:17 -0400

    Test stuff

---
+++ tests/_components/Vendor.test.tsx
@@ -0,0 +1,8 @@
+import {render} from '@testing-library/react'
+import Header from '../../app/_components/Header'
+
+describe('Header.tsx', () => {
+    test('renders', () => {
+        expect(1).toEqual(1)
+    })
+})
