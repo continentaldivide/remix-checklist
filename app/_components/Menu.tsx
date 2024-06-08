56edeccd6261018618e10c0c8ee9fb2f7e627f79
Andrew <andrew@andrewsmith.org>
2024-06-08 13:17:01 -0400

    Componentize menu options

--- app/_components/Menu.tsx
+++ app/_components/Menu.tsx
@@ -1,44 +1,27 @@
-import SwitchComponent from "./SwitchComponent";
+import MenuOption from "./MenuOption";
 import { useAppStateContext } from "../_context/AppStateContext";
 
 export default function Menu() {
   const { appState, appStateDispatch } = useAppStateContext();
   return (
-    <div className="absolute top-12 left-0 z-50">
-      <div className="odd:bg-gray-600 even:bg-gray-700 flex justify-between items-center">
-        <button
-          onClick={(e) => {
-            e.stopPropagation();
-            appStateDispatch({ type: "toggle ignore", category: "nonEvent" });
-          }}
-          className="my-auto hover:brightness-125"
-        >
-          show only event exclusives
-        </button>
-        <SwitchComponent
-          checked={appState.ignoredItems.nonEvent}
-          onChange={() => {
-            appStateDispatch({ type: "toggle ignore", category: "nonEvent" });
-          }}
-        />
-      </div>
-      <div className="odd:bg-gray-600 even:bg-gray-700 flex justify-between items-center">
-        <button
-          onClick={(e) => {
-            e.stopPropagation();
-            appStateDispatch({ type: "toggle ignore", category: "obtained" });
-          }}
-          className="my-auto hover:brightness-125"
-        >
-          Hide checked items
-        </button>
-        <SwitchComponent
-          checked={appState.ignoredItems.obtained}
-          onChange={() => {
-            appStateDispatch({ type: "toggle ignore", category: "obtained" });
-          }}
-        />
-      </div>
+    <div className="absolute top-14 left-2 z-50 rounded-md">
+      <MenuOption
+        checked={appState.ignoredItems.nonEvent}
+        label="show only event exclusives"
+        dispatch={() =>
+          appStateDispatch({
+            type: "toggle ignore",
+            category: "nonEvent",
+          })
+        }
+      />
+      <MenuOption
+        checked={appState.ignoredItems.obtained}
+        label="hide checked items"
+        dispatch={() =>
+          appStateDispatch({ type: "toggle ignore", category: "obtained" })
+        }
+      />
     </div>
   );
 }
