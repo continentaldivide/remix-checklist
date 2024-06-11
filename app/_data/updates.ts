34c05d69448441c13c73f79b1fc50990246fdd95
Andrew <andrew@andrewsmith.org>
2024-06-11 19:52:16 -0400

    Add update description for 1.0.1

--- app/_data/updates.ts
+++ app/_data/updates.ts
@@ -3,65 +3,11 @@ import { UpdateType } from "../_interfaces/Update.interface";
 const updates: UpdateType[] = [
   {
     version: "1.0.1",
-    releaseDate: "May 21, 2024",
+    releaseDate: "June 11, 2024",
     changes: [
-      "Added a news window to inform users about new features.",
-      "Added a second line here as dummy text to try and ensure the styling looks good.",
-      "Some changes might take up several megalines on mobile, so I want to make sure I have an extra long change here as well.",
-    ],
-  },
-  {
-    version: "1.0.1",
-    releaseDate: "May 21, 2024",
-    changes: [
-      "Added a news window to inform users about new features.",
-      "Added a second line here as dummy text to try and ensure the styling looks good.",
-      "Some changes might take up several lines on mobile, so I want to make sure I have an extra long change here as well.",
-    ],
-  },
-  {
-    version: "1.0.1",
-    releaseDate: "May 21, 2024",
-    changes: [
-      "Added a news window to inform users about new features.",
-      "Added a second line here as dummy text to try and ensure the styling looks good.",
-      "Some changes might take up several lines on mobile, so I want to make sure I have an extra long change here as well.",
-    ],
-  },
-  {
-    version: "1.0.1",
-    releaseDate: "May 21, 2024",
-    changes: [
-      "Added a news window to inform users about new features.",
-      "Added a second line here as dummy text to try and ensure the styling looks good.",
-      "Some changes might take up several lines on mobile, so I want to make sure I have an extra long change here as well.",
-    ],
-  },
-  {
-    version: "1.0.1",
-    releaseDate: "May 21, 2024",
-    changes: [
-      "Added a news window to inform users about new features.",
-      "Added a second line here as dummy text to try and ensure the styling looks good.",
-      "Some changes might take up several lines on mobile, so I want to make sure I have an extra long change here as well.",
-    ],
-  },
-  {
-    version: "1.0.1",
-    releaseDate: "May 21, 2024",
-    changes: [
-      "Added a news window to inform users about new features.",
-      "Added a second line here as dummy text to try and ensure the styling looks good.",
-      "Some changes might take up several lines on mobile, so I want to make sure I have an extra long change here as well.",
-    ],
-  },
-  {
-    version: "1.0.1",
-    releaseDate: "May 21, 2024",
-    changes: [
-      "Added a news window to inform users about new features.",
-      "Added a second line here as dummy text to try and ensure the styling looks good.",
-      "Some changes might take up several lines on mobile, so I want to make sure I have an extra long change here as well.",
+      "Added a menu with some new options for filtering out items.",
+      "Added Theramore Tabard and fixed Mini Mana Bomb cost.",
+      "Fixed some typos and issues in the code.  (Thanks Rolf!)",
     ],
   },
 ];
