4f3ea2ab3ffa693974675b9b384d0b22ea68a854
Andrew <andrew@andrewsmith.org>
2024-06-28 01:00:35 -0400

    Finalize release date for v1.0.1

--- app/_data/updates.ts
+++ app/_data/updates.ts
@@ -3,7 +3,7 @@ import { UpdateType } from "../_interfaces/Update.interface";
 const updates: UpdateType[] = [
   {
     version: "1.0.1",
-    releaseDate: "June 11, 2024",
+    releaseDate: "June 28, 2024",
     changes: [
       "Added a menu with some new options for filtering out items.",
       "Added Theramore Tabard and fixed Mini Mana Bomb cost.",
