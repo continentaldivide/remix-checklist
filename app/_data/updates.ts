90ee48272caadc8c963935a05627e2f4501b13e7
Andrew <andrew@andrewsmith.org>
2025-10-04 22:23:44 -0400

    Update version

--- app/_data/updates.ts
+++ app/_data/updates.ts
@@ -2,6 +2,11 @@ import { UpdateType } from "../_interfaces/Update.interface";
 
 const updates: UpdateType[] = [
   {
+    version: "2.0.0",
+    releaseDate: "October 4, 2025",
+    changes: ["Updated the app for Legion Remix."],
+  },
+  {
     version: "1.0.1",
     releaseDate: "June 28, 2024",
     changes: [
