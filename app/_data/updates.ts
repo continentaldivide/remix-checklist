998363007165e2f11ab00c56917a31f0d071ff25
Andrew <andrew@andrewsmith.org>
2025-10-07 19:33:45 -0400

    Update vendors

--- app/_data/updates.ts
+++ app/_data/updates.ts
@@ -2,6 +2,18 @@ import { UpdateType } from "../_interfaces/Update.interface";
 
 const updates: UpdateType[] = [
   {
+    version: "2.0.2",
+    releaseDate: "October 7, 2025",
+    changes: [
+      "Updated vendors to match live event:",
+      "Added Emerald Drake Hunter's Kit to Agos the Silent.",
+      "Removed Scavenged Chains of Karazhan from Larah Treebender.",
+      "Removed Gems of the Lightforged Draenei from Unicus.",
+      "Removed Tylarr Gronnden from Horos.",
+      "Reduced costs of some mounts sold by Hemet Nesingwary XVII.",
+    ],
+  },
+  {
     version: "2.0.1",
     releaseDate: "October 7, 2025",
     changes: [
